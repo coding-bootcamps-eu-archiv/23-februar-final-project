a13b0f3497e8417ac5fa1c3e4f237b66ced99873
¨RaBe¨ <raphaela.becker@posteo.de>
2023-05-12 10:27:51 +0200

    feat: AddButton

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,15 @@
 <template>
+  <BaseButton>blub</BaseButton>
   <h1>New Project</h1>
 </template>
 
 <script>
+import BaseButton from "@/components/BaseButton.vue";
+
 export default {
   name: "HomeView",
-  components: {},
+  components: {
+    BaseButton,
+  },
 };
 </script>
