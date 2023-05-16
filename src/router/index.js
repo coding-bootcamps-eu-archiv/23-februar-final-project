33d2cd7a11325870b3e3afce6e921e3aab352ced
¨RaBe¨ <raphaela.becker@posteo.de>
2023-05-16 13:21:34 +0200

    feat:add-plantview

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ const routes = [
     name: "home",
     component: HomeView,
   },
+  {
+    path: "/plant",
+    name: "plant",
+    component: () => import("../views/PlantView"),
+  },
 ];
 
 const router = createRouter({
